b22b9874ed442f58af989260b9924b6f29c6b71d
SuVao <pedro.radon@gmail.com>
2024-04-02 14:45:04 +0100

    ft_memcpy_bzero

---
+++ bibliotecas/libft/ft_bzero.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+void	ft_bzero(void *str, size_t	num)
+{
+	unsigned char	*char_str;
+	size_t					i;
+
+	i = 0;
+	char_str = str;
+	while (i < num)
+		char_str[i++] = '\0';
+}
+
+/*int main () {
+   char str[50];
+
+   strcpy(str,"This is string.h library function");
+   puts(str);
+
+   ft_bzero(str + 3, 1);
+   puts(str);
+
+   return(0);
+}*/
