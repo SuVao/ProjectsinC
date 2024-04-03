d4478cdf829449d62f3ba115a5de51ca024136b5
SuVao <pedro.radon@gmail.com>
2024-04-03 15:47:22 +0100

    fm_memmove.c

---
+++ bibliotecas/libft/ft_memmove.c
@@ -0,0 +1,30 @@
+#include <string.h>
+#include <stdio.h>
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	const unsigned char		*char_src;
+	unsigned char		*char_dest;
+	size_t				i;
+
+	i = 0;
+	char_src = src;
+	char_dest = dest;
+	while (i < n)
+	{
+		char_dest[i] = char_src[i];
+		i++;
+	}
+	return (dest);
+}
+
+/*int main () {
+   char dest[] = "old string";
+   const char src[]  = "fuck string";
+
+   printf("Before memmove dest = %s, src = %s\n", dest, src);
+   ft_memmove(dest, src, 5);
+   printf("After memmove dest = %s, src = %s\n", dest, src);
+
+   return(0);
+}*/
